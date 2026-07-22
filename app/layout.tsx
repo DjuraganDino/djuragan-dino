4a8e3d3d76c11ecab61dfa9cd30ab2ea5279f1e5
DjuraganDino <rizkip7210@gmail.com>
2026-07-23 03:33:03 +0700

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
       >
         {children}
 
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-G9LC9DB2ZN"
           strategy="afterInteractive"
@@ -46,6 +47,20 @@ export default function RootLayout({
 
             gtag('js', new Date());
             gtag('config', 'G-G9LC9DB2ZN');
+          `}
+        </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xqlpqwyvyg");
           `}
         </Script>
       </body>
