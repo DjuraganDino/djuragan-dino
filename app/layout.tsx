3a01f4360540c5a1253872b2099ddc309a2218b5
DjuraganDino <rizkip7210@gmail.com>
2026-07-23 04:05:42 +0700

    Fix Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
+      <head>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xqlpqwyvyg");
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#08111E] text-white`}
       >
@@ -48,12 +61,6 @@ export default function RootLayout({
             gtag('config', 'G-G9LC9DB2ZN');
           `}
         </Script>
-
-        {/* Microsoft Clarity */}
-        <Script
-          src="https://www.clarity.ms/tag/xqlpqwyvyg"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
